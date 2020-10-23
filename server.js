0fcf6dbb1acd2f35f86f333f69eb5d5721315a4e
JosMuse <no-reply>
2020-10-23 13:16:57 -0600

    most everything is done I believe

--- server.js
+++ server.js
@@ -1,86 +1,86 @@
-const express = require('express')
-const fs = require("fs")
-const path = require("path")
-const uuid = require("uuid")
-const app = express()
-const port = process.env.PORT || 3000
 
+//DEPENDENCIES ============================================
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const uuid = require("uuid");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+//============================================
 
-//middleware
-app.use(express.static("public"))
+//middleware ============================================
+app.use(express.static("public")); //this get index.html and notes.html
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// ============================================
 
 
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+//routes ============================================
 
-
-//routes
-app.get('/api/notes', (req, res) => {
-
-  fs.readFile(path.join(__dirname,'','./db/db.json'), (err, data) => {
-    if (err){
-      throw err
+//Get to get the notes info
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "", "./db/db.json"), (err, data) => {
+    if (err) {
+      throw err;
     }
-    const notes = JSON.parse(data)
-    res.json(notes)
-  })
- 
-})
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
 
-app.post("/api/notes", function(req, res){
-
-  fs.readFile(path.join(__dirname, './db/db.json'), (err, data)  => { 
-
-    if (err){
-      throw err
+//============================================
+//post to create new note
+app.post("/api/notes", function (req, res) {
+  fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+    if (err) {
+      throw err;
     }
 
-    const newNote =  JSON.parse(data)
-    const newNote2 = req.body
-    newNote2.id = uuid.v4() 
-    newNote.push(newNote2)
+    const newNote = JSON.parse(data);
+    console.log(newNote);
+    const newNote2 = req.body;
+    newNote2.id = uuid.v4();
+    newNote.push(newNote2);
 
-    const finalNewNote = JSON.stringify(newNote)
+    const finalNewNote = JSON.stringify(newNote);
     fs.writeFile(path.join(__dirname, "./db/db.json"), finalNewNote, (err) => {
-      if (err){
-        throw err
+      if (err) {
+        throw err;
       }
-    })
-    res.json(newNote2)
+    });
+    res.json(newNote2);
+  });
+});
 
-  })
-
-})
-
-app.delete("/api/notes/:id" ,function(req, res){
-  const uniqueID =  req.params.id
-  fs.readFile(path.join(__dirname,'','./db/db.json') ,function(err, data){
-    if (err){
-      throw err
+//============================================
+//delete to delete note from saved notes
+app.delete("/api/notes/:id", function (req, res) {
+  const uniqueID = req.params.id;
+  fs.readFile(path.join(__dirname, "", "./db/db.json"), function (err, data) {
+    if (err) {
+      throw err;
     }
-    const notes = JSON.parse(data)
-    const noteDelete = notes.filter(
-      (noteObj) => noteObj.id !== uniqueID
-    );
+    const notes = JSON.parse(data); //make object
+    const noteDelete = notes.filter((noteObj) => noteObj.id !== uniqueID);
 
-    fs.writeFile("./db/db.json", JSON.stringify(noteDelete), function (err, data){
-      if (err){
-        throw err
+    fs.writeFile("./db/db.json", JSON.stringify(noteDelete), function (err) {
+      if (err) {
+        throw err;
       }
-    })
+    });
+    res.json(noteDelete);
+  });
+});
+//============================================
+//============================================
 
-  })
-})
 
-//listeners
+//listeners ============================================
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
+// ============================================
